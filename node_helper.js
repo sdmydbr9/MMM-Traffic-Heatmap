e661402af5888a4b47a0958cf97508109e028cb9
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-11 10:59:47 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,5 +1,5 @@
 const NodeHelper = require("node_helper");
-const fetch = require("node-fetch");
+const fetch = await import('node-fetch');
 
 module.exports = NodeHelper.create({
   start: function() {
